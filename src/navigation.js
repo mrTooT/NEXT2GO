3ca73cd3cf646346dd45c158244bdf9ffdcbde5d
Rik Toetenel <rik.toetenel@randstadgroep.nl>
2024-01-18 18:52:23 +0100

    Remove announcement && change menu

--- src/navigation.js
+++ src/navigation.js
@@ -3,28 +3,7 @@ import { getPermalink, getBlogPermalink, getAsset } from './utils/permalinks';
 export const headerData = {
   links: [
     {
-      text: 'Homes',
-      links: [
-        {
-          text: 'SaaS',
-          href: getPermalink('/homes/saas'),
-        },
-        {
-          text: 'Startup',
-          href: getPermalink('/homes/startup'),
-        },
-        {
-          text: 'Mobile App',
-          href: getPermalink('/homes/mobile-app'),
-        },
-        {
-          text: 'Personal',
-          href: getPermalink('/homes/personal'),
-        },
-      ],
-    },
-    {
-      text: 'Pages',
+      text: 'Mick is een beetje gek',
       links: [
         {
           text: 'Features (Anchor Link)',

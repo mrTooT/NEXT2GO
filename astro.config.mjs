cf423a0bcb797fa073fb026ac9148f6a6e83e380
Rik Toetenel <rik.toetenel@randstadgroep.nl>
2024-04-26 13:07:46 +0200

    Fix Contact Formn

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,15 +9,20 @@ import icon from 'astro-icon';
 import tasks from './src/utils/tasks';
 import { readingTimeRemarkPlugin, responsiveTablesRehypePlugin } from './src/utils/frontmatter.mjs';
 import { ANALYTICS, SITE } from './src/utils/config.ts';
-import react from "@astrojs/react";
+import react from '@astrojs/react';
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
-const whenExternalScripts = (items = []) => ANALYTICS.vendors.googleAnalytics.id && ANALYTICS.vendors.googleAnalytics.partytown ? Array.isArray(items) ? items.map(item => item()) : [items()] : [];
+const whenExternalScripts = (items = []) =>
+  ANALYTICS.vendors.googleAnalytics.id && ANALYTICS.vendors.googleAnalytics.partytown
+    ? Array.isArray(items)
+      ? items.map((item) => item())
+      : [items()]
+    : [];
 
 // Loading environment variables from .env files
 // https://docs.astro.build/en/guides/configuring-astro/#environment-variables
-import { sanityIntegration as sanity } from "@sanity/astro";
+import { sanityIntegration as sanity } from '@sanity/astro';
 
-import netlify from "@astrojs/netlify";
+import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
@@ -25,36 +30,57 @@ export default defineConfig({
   base: SITE.base,
   trailingSlash: SITE.trailingSlash ? 'always' : 'never',
   output: 'hybrid',
-  integrations: [sanity({
-    projectId: "5eo5uf46",
-    dataset: "production",
-    studioBasePath: "/admin",
-    useCdn: false
-  }), tailwind({
-    applyBaseStyles: false
-  }), sitemap(), mdx(), icon({
-    include: {
-      tabler: ['*'],
-      'flat-color-icons': ['template', 'gallery', 'approval', 'document', 'advertising', 'currency-exchange', 'voice-presentation', 'business-contact', 'database']
-    }
-  }), ...whenExternalScripts(() => partytown({
-    config: {
-      forward: ['dataLayer.push']
-    }
-  })), tasks(), react()],
+  integrations: [
+    sanity({
+      projectId: '5eo5uf46',
+      dataset: 'production',
+      studioBasePath: '/admin',
+      useCdn: false,
+    }),
+    tailwind({
+      applyBaseStyles: false,
+    }),
+    sitemap(),
+    mdx(),
+    icon({
+      include: {
+        tabler: ['*'],
+        'flat-color-icons': [
+          'template',
+          'gallery',
+          'approval',
+          'document',
+          'advertising',
+          'currency-exchange',
+          'voice-presentation',
+          'business-contact',
+          'database',
+        ],
+      },
+    }),
+    ...whenExternalScripts(() =>
+      partytown({
+        config: {
+          forward: ['dataLayer.push'],
+        },
+      })
+    ),
+    tasks(),
+    react(),
+  ],
   image: {
-    service: squooshImageService()
+    service: squooshImageService(),
   },
   markdown: {
     remarkPlugins: [readingTimeRemarkPlugin],
-    rehypePlugins: [responsiveTablesRehypePlugin]
+    rehypePlugins: [responsiveTablesRehypePlugin],
   },
   vite: {
     resolve: {
       alias: {
-        '~': path.resolve(__dirname, './src')
-      }
-    }
+        '~': path.resolve(__dirname, './src'),
+      },
+    },
   },
-  adapter: netlify()
+  adapter: netlify(),
 });
--- src/components/quiz/Quiz.tsx
+++ src/components/quiz/Quiz.tsx
@@ -4,6 +4,8 @@ import RAnswer from '~/components/quiz/Answer.tsx';
 import { client } from '~/client';
 import imageUrlBuilder from '@sanity/image-url';
 import { PortableText } from '@portabletext/react';
+import sendgrid from '@sendgrid/mail';
+import { useForm } from 'react-hook-form';
 
 export interface Props {
   title?: string;
@@ -27,6 +29,7 @@ const urlFor = (source) => {
 };
 
 const RQuiz = (props: Props) => {
+  const { register, handleSubmit } = useForm();
   const [questions, setQuestions] = useState<Question[]>([]);
   const [content, setContent] = useState<Content>();
   const [answeredQuestions, setAnsweredQuestions] = useState<Question[]>([]);
@@ -67,12 +70,87 @@ const RQuiz = (props: Props) => {
   const [activeQuestion, setActiveQuestion] = useState<Question>();
   const [activeQuiz, setActiveQuiz] = useState(false);
 
+  const getEmailTemplate = (data) => {
+    return {
+      to: 'riktoet@gmail.com', // Your email where you'll receive emails
+      from: 'hello@tipmytravel.com', // your website email address here
+      subject: `[Contact formulier ingevuld door: ] : ${data.name}`,
+      html: `<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+    <html lang="en">
+    <head>
+      <meta charset="utf-8">
+    
+      <title>The HTML5 Herald</title>
+      <meta name="description" content="The HTML5 Herald">
+      <meta name="author" content="SitePoint">
+    <meta http-equiv="Content-Type" content="text/html charset=UTF-8" />
+    
+      <link rel="stylesheet" href="css/styles.css?v=1.0">
+    
+    </head>
+    
+    <body>
+      <div class="img-container" style="display: flex;justify-content: center;align-items: center;border-radius: 5px;overflow: hidden; font-family: 'helvetica', 'ui-sans';">              
+            </div>
+            <div class="container" style="margin-left: 20px;margin-right: 20px;">
+            <h3>Hoooooooi Mickjem You've got a new mail from ${data.name}, their email is: ✉️${data.email} </h3>
+            <div style="font-size: 16px;">
+            <p>Name:</p>
+            <p>${data.name}</p>
+            <p>Email:</p>
+            <p>${data.email}</p>
+            <p>Phone:</p>
+            <p>${data.phone}</p>
+            <p>Message:</p>
+            <p>${data.message}</p>
+            <br>
+            </div>
+            <img src="https://manuarora.in/logo.png" class="logo-image" style="height: 50px;width: 50px;border-radius: 5px;overflow: hidden;">
+            <p class="footer" style="font-size: 16px;padding-bottom: 20px;border-bottom: 1px solid #D1D5DB;">Regards<br>Manu Arora<br>Software Developer<br>+91 9587738861</p>
+            <div class="footer-links" style="display: flex;justify-content: center;align-items: center;">
+              <a href="https://manuarora.in/" style="text-decoration: none;margin: 8px;color: #9CA3AF;">Website</a>
+              <a href="https://manuarora.in/blog/" style="text-decoration: none;margin: 8px;color: #9CA3AF;">Blog</a>
+              <a href="https://github.com/manuarora700/" style="text-decoration: none;margin: 8px;color: #9CA3AF;">GitHub</a>
+              <a href="https://instagram.com/maninthere/" style="text-decoration: none;margin: 8px;color: #9CA3AF;">Instagram</a>
+              <a href="https://linkedin.com/in/manuarora28/" style="text-decoration: none;margin: 8px;color: #9CA3AF;">LinkedIn</a>
+              <a href="https://twitter.com/mannupaaji/" style="text-decoration: none;margin: 8px;color: #9CA3AF;">Twitter</a>
+              
+            </div>
+            </div>
+    </body>
+    </html>`,
+    };
+  };
+
+  const handleFormSubmit = async (data) => {
+    const apikey = 'SG.4AkHyPqoS4KD3JXNZIdhfg.oHY2DaD9Ed8L3NNYEoarYdkCA9jaYP8AHYR2jqhfkCs';
+    sendgrid.setApiKey(apikey);
+    sendgrid.send(getEmailTemplate(data));
+  };
+
   const scrollIntoView = () => {
     if (activeQuestion && activeQuestionsRef?.current) {
       activeQuestionsRef?.current.scrollIntoView();
     } else if (!activeQuestion && answeredQuestionsRef?.current) {
       answeredQuestionsRef?.current.scrollIntoView();
     }
+  };
+
+  const renderInput = (name, type, placeholder) => {
+    return (
+      <div className="mb-6">
+        <label htmlFor={name} className="block text-sm font-medium"></label>
+        <input
+          {...register(name)}
+          type={type}
+          name={name}
+          id={name}
+          placeholder={placeholder}
+          required
+          className="py-3 px-4 block w-full text-md rounded-lg border border-gray-200 dark:border-gray-700 bg-white dark:bg-slate-900"
+        />
+      </div>
+    );
   };
 
   const RenderAnsweredQuestions = () => {
@@ -104,7 +182,7 @@ const RQuiz = (props: Props) => {
             )
         )}
         <a className="edit-link capitalize hover:underline cursor-pointer" onClick={() => handleCloseQuiz(false)}>
-          edit
+          Edit
         </a>
       </div>
     );
@@ -147,7 +225,32 @@ const RQuiz = (props: Props) => {
             {content?.quizFinishedTitle}
           </h2>
           {content?.quizFinishedText && <PortableText value={content?.quizFinishedText} />}
-          {props.children}
+          <div className="mt-4 md:mx-auto md:mb-12 text-center max-w-3xl">
+            <form id="contact" name="contact" method="GET" onSubmit={handleSubmit((data) => handleFormSubmit(data))}>
+              <input type="hidden" name="form-name" value="contact" />
+              {renderInput('name', 'name', 'Your name')}
+              {renderInput('email', 'email', 'Your email')}
+              {renderInput('phone', 'phone', 'Your phonenumber')}
+              <div>
+                <label htmlFor="textarea" className="block text-sm font-medium">
+                  Message
+                </label>
+                <textarea
+                  {...register('message')}
+                  id="textarea"
+                  rows={4}
+                  placeholder="Your message"
+                  required
+                  className="py-3 px-4 block w-full text-md rounded-lg border border-gray-200 dark:border-gray-700 bg-white dark:bg-slate-900"
+                />
+              </div>
+              <div className="mt-10 grid">
+                <button id="form-submit" className="btn-primary" type="submit">
+                  Contact us
+                </button>
+              </div>
+            </form>
+          </div>
         </div>
       )
     );

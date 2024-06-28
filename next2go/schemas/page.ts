14f45a408012e4720123a8d01c6e3d19221043a5
Rik Toetenel <rik.toetenel@randstadgroep.nl>
2024-06-28 14:52:48 +0200

    try links

--- next2go/schemas/page.ts
+++ next2go/schemas/page.ts
@@ -83,6 +83,44 @@ export default defineType({
       of: [
         {
           type: 'block',
+          marks: {
+            annotations: [
+              {
+                name: 'link',
+                type: 'object',
+                title: 'External link',
+                fields: [
+                  {
+                    name: 'href',
+                    type: 'url',
+                    title: 'URL',
+                  },
+                  {
+                    title: 'Open in new tab',
+                    name: 'blank',
+                    description: 'Read https://css-tricks.com/use-target_blank/',
+                    type: 'boolean',
+                  },
+                ],
+              },
+              {
+                name: 'internalLink',
+                type: 'object',
+                title: 'Internal link',
+                fields: [
+                  {
+                    name: 'reference',
+                    type: 'reference',
+                    title: 'Reference',
+                    to: [
+                      {type: 'post'},
+                      // other types you may want to link to
+                    ],
+                  },
+                ],
+              },
+            ],
+          },
         },
       ],
     }),
